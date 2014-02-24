e434ff81b406cebbcf3d79df69a019a8f60ed5f7
Michael Seghers <michael.seghers@ida-mediafoundry.be>
2014-02-24 22:23:09 +0100

    Added some extra labels to showcase reading values from the info plist

--- ConfigApp/ConfigApp/Controllers/DRYShowConfigurationViewController.m
+++ ConfigApp/ConfigApp/Controllers/DRYShowConfigurationViewController.m
@@ -8,39 +8,58 @@
 
 #import "DRYShowConfigurationViewController.h"
 
-@interface DRYShowConfigurationViewController ()
-
+@interface DRYShowConfigurationViewController () {
+    NSDictionary *_infoDictionary;
+}
+    
 @end
 
 @implementation DRYShowConfigurationViewController
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
+    
+- (id)init {
+    self = [super init];
+    if (self) {
+        _infoDictionary = [[NSBundle mainBundle] infoDictionary];
+    }
+    return self;
+}
+    
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
     }
     return self;
 }
-
-- (void)viewDidLoad
-{
+    
+- (void)viewDidLoad {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     self.view.translatesAutoresizingMaskIntoConstraints = NO;
     
     
-    UILabel *valueFromConfigurationFileLabel = [[UILabel alloc] init];
-    valueFromConfigurationFileLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    UILabel *valueFromConfigurationFileLabel = [self createInfoLabel];
     valueFromConfigurationFileLabel.text = [self readValueFromConfigurationFile];
-    valueFromConfigurationFileLabel.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:valueFromConfigurationFileLabel];
-
+    UILabel *appVersionLabel = [self createInfoLabel];
+    appVersionLabel.text = [NSString stringWithFormat:@"App bundle version: %@", _infoDictionary[@"CFBundleVersion"]];
+    UILabel *bundleIdLabel = [self createInfoLabel];
+    bundleIdLabel.text = [NSString stringWithFormat:@"App bundle identifier: %@", _infoDictionary[@"CFBundleIdentifier"]];
     
-    NSDictionary *views = NSDictionaryOfVariableBindings(valueFromConfigurationFileLabel);
+    NSDictionary *views = NSDictionaryOfVariableBindings(valueFromConfigurationFileLabel, appVersionLabel, bundleIdLabel);
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-[valueFromConfigurationFileLabel]-|" options:0 metrics:nil views:views]];
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[valueFromConfigurationFileLabel]-|" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-[appVersionLabel]-|" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"|-[bundleIdLabel]-|" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-[valueFromConfigurationFileLabel]-[appVersionLabel]-[bundleIdLabel]-|" options:0 metrics:nil views:views]];
     
+}
+    
+- (UILabel *) createInfoLabel {
+    UILabel *label = [[UILabel alloc] init];
+    label.translatesAutoresizingMaskIntoConstraints = NO;
+    label.textAlignment = NSTextAlignmentCenter;
+    label.adjustsFontSizeToFitWidth = YES;
+    [self.view addSubview:label];
+    return label;
 }
     
 - (NSString *) readValueFromConfigurationFile {
@@ -48,11 +67,10 @@
     NSDictionary *configDictionary = [NSDictionary dictionaryWithContentsOfFile:configPath];
     return configDictionary[@"configParameter"];
 }
-
-- (void)didReceiveMemoryWarning
-{
+    
+- (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+    
 @end
