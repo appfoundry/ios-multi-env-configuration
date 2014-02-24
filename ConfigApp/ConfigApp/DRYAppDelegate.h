60d475d0b7154bac636ed570d5d02e19e7acdbf3
Michael Seghers <michael.seghers@ida-mediafoundry.be>
2014-02-24 21:29:16 +0100

    Created blank application

---
+++ ConfigApp/ConfigApp/DRYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DRYAppDelegate.h
+//  ConfigApp
+//
+//  Created by Michael Seghers on 24/02/14.
+//  Copyright (c) 2014 Michael Seghers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DRYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ConfigApp/ConfigApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ConfigApp
+//
+//  Created by Michael Seghers on 24/02/14.
+//  Copyright (c) 2014 Michael Seghers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DRYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DRYAppDelegate class]));
+    }
+}
